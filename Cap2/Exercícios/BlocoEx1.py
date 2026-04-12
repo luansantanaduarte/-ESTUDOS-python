dee36f19d96dd75880a8c0f244238de974a578d8
Luan <lsantana.duarte7@gmail.com>
2026-04-12 20:39:07 -0300

    iniciando repositório

---
+++ Cap2/Exercícios/BlocoEx1.py
@@ -0,0 +1,16 @@
+# BLOCO DE EXERCÍCIOS 1
+
+#  Exerício 2.1 - Converta as seguintes operações de modo que possam ser calculadas pelo interpretador de Python
+# ? lembrando que os operadores são: + (soma), - (subtração), * (multiplicação), / (divisão), // (divisão inteira), ** (exponenciação) 
+
+# a) 10 + 20 x 30
+print(10 + 20 * 30)
+
+# b) 4² ÷ 30
+print(4 ** 2 / 30)
+
+# c) (9^4 + 2 ) x 6 - 1
+print((9 ** 4 + 2) * 6 - 1)
+
+# Exerício 2.2 - Determine a seguinte expressão no interpretador
+print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
---
+++ Cap2/Exercícios/BlocoEx2.py
@@ -0,0 +1,19 @@
+# BLOCO DE EXERÍCIOS 2
+
+# Exercício 2.3 - Faça um programa que exiba o seu nome na tela
+print("luan")
+
+# Exercício 2.4 - Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5
+a = 3 
+b = 5
+resultado = (2 * a) * (3 * b)
+print(resultado)
+
+# Exercício 2.5 - Escreva um programa que calcule a soma de três váriaveis e imprima o resultado na tela
+a = 9
+b = 8
+c = 2
+print(a + b + c)  
+
+
+
