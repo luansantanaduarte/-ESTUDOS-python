b95da0a5ed9b800a44d159c3981eba829b22145a
Luan <lsantana.duarte7@gmail.com>
2026-04-14 14:38:09 -0300

    alterando nome das pastas e tornando mais explícito os assuntos vistos

---
+++ Cap2PreparandoOAmbiente/Intro1.py
@@ -0,0 +1,17 @@
+# Introdução a lógica de Programação com Python - Capítulo 2
+
+# * Função PRINT -> Exibe algum elemento na tela 
+# ? Estrutura: print(parâmetros) 
+# OBS.: O uso das aspas se dá pelo fato de que oi é um texto. As aspas dividem textos do resto do código.
+print("Oi")
+
+# ! Conceito de váriaveis e atribuição
+# * Variáveis -> Armazenam dados e dão nome a uma área da memória do comuputador (= -> Recebe)
+# ? Sintaxe: nome_da_variavel = dado (nome_da_variavel recebe dado)
+a = 2 # a recebe 2
+b = 3 # b recebe 3
+print(a + b)
+
+# Utilizamos varíaveis com o objetivo de facilitar am udança de parâmetros e valores;
+# Para melhorar a legibilidade do código, o ideal é que o nome seja o mais específico possível.
+
